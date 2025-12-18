daf842ab94d6f689d7eaa082e898c52fb8b0c798
2EFF <145107218+2EFFE@users.noreply.github.com>
2025-12-18 13:30:13 +0100

    feat: Add Node.js types and update Vite config
    Includes `@types/node` for better TypeScript support and sets the Vite development server port to 3000 for clarity.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -13,5 +11,8 @@ export default defineConfig({
         main: './index.html'
       }
     }
+  },
+  server: {
+    port: 3000
   }
 });
